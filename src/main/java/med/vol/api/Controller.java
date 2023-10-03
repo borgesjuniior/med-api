bd787a8b724cd447268c83e0b33d578a9aa263ef
Valdecy Júnior <valdecy.costa@compasso.com.br>
2023-10-03 17:44:30 -0300

    spring initializr

---
+++ src/main/java/med/vol/api/Controller.java
@@ -0,0 +1,15 @@
+package med.vol.api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("hello")
+public class Controller {
+
+  @GetMapping
+  public String helloWorld() {
+    return "Hello World!";
+  };
+}
