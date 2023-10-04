67f5601ce16371b725c509ea63bb1ad5f798a1a0
Valdecy Júnior <valdecy.costa@compasso.com.br>
2023-10-04 16:39:50 -0300

    pagination doctors list with spring

--- src/main/java/med/vol/api/Controllers/DoctorController.java
+++ src/main/java/med/vol/api/Controllers/DoctorController.java
@@ -1,8 +1,8 @@
 package med.vol.api.Controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +31,7 @@ public class DoctorController {
   }
 
   @GetMapping
-  public List<MedicalListDTO> list() {
-    return doctorReposity.findAll().stream().map(MedicalListDTO::new).toList();
+  public Page<MedicalListDTO> list(Pageable pageable) {
+    return doctorReposity.findAll(pageable).map(MedicalListDTO::new);
   }
 }
