baae9faa0cb04137f0c665cb2632df0ee50a88e9
Junior <valdecyborgesjr@hotmail.com>
2023-10-08 19:27:50 -0300

    spring security configuration

--- src/main/java/med/vol/api/repositories/UserRepository.java
+++ src/main/java/med/vol/api/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package med.vol.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import med.vol.api.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  UserDetails findByLogin(String login);
 
 }
