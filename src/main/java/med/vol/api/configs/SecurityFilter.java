4985a9ebdf49b61fc22abe072ad39a90b1daaab6
Valdecy Júnior <valdecy.costa@compasso.com.br>
2023-10-09 21:30:57 -0300

    user authentication

--- src/main/java/med/vol/api/configs/SecurityFilter.java
+++ src/main/java/med/vol/api/configs/SecurityFilter.java
@@ -3,6 +3,8 @@ package med.vol.api.configs;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,6 +12,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.vol.api.repositories.UserRepository;
 import med.vol.api.services.TokenService;
 
 @Component // generic component from Spring
@@ -18,22 +21,33 @@ public class SecurityFilter extends OncePerRequestFilter {
   @Autowired
   private TokenService tokenService;
 
+  @Autowired
+  private UserRepository userRepository;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
     var token = getToken(request);
-    tokenService.getSubject(token);
+
+    if (token != null) {
+      var subject = tokenService.getSubject(token);
+      var user = userRepository.findByLogin(subject);
+
+      var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+      SecurityContextHolder.getContext().setAuthentication(authentication); // Authenticate user
+    }
+
     filterChain.doFilter(request, response);
   }
 
   private String getToken(HttpServletRequest request) {
     var authorizationHeader = request.getHeader("Authorization");
 
-    if (authorizationHeader == null) {
-      throw new RuntimeException("JWT Token not found");
+    if (authorizationHeader != null) {
+      return authorizationHeader.replace("Bearer ", "");
     }
 
-    return authorizationHeader.replace("Bearer ", "");
+    return null;
   }
 
 }
