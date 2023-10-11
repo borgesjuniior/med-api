2833ac0a86cd0ee8fe170e2844c00b88febbf9b4
unknown <valdecyborgesjr@hotmail.com>
2023-10-11 15:21:04 -0300

    add patient entity

---
+++ src/main/java/med/vol/api/entities/Patient.java
@@ -0,0 +1,31 @@
+package med.vol.api.entities;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Patient")
+@Table(name = "patients")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Patient {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+  private String name;
+  private String email;
+  private String cpf;
+
+  @Embedded
+  private Address address;
+}
